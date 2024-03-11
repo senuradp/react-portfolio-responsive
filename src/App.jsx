9bfe284303a101f2043c3be73877f22d0861bb53
Senura Perera <senura.perera@arimaclanka.com>
2024-03-11 23:45:56 +0530

    Services section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
 import Home from './components/home/Home.jsx'
+import Services from './components/services/Services.jsx'
 
 function App() {
 
   return (
     <main className='main'>
       <Home />
+      <Services />
     </main>
   )
 }
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,69 @@
+/* eslint-disable no-undef */
+/* eslint-disable react/jsx-key */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './services.css'
+import {services} from '../../Data'
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+
+import { FaArrowRight } from 'react-icons/fa';
+
+import shapeTwo from "../../assets/shape-2.png";
+
+const Services = () => {
+  return (
+    <section className="services section" id="services">
+        <h2 className="section__title text-cs">
+            What I Do
+        </h2>
+        <p className="section__subtitle">
+            My <span>Services</span>
+        </p>
+
+        <Swiper 
+        className="services__container container mySwiper"
+        pagination={{
+            clickable: true,
+        }}
+        breakpoints={{
+            540: {
+                slidesPerView: 1,
+                spaceBetween: 30,
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 30,
+            },
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+            },
+        }}
+        modules={[Pagination]}
+        >
+
+            {services.map(({name, title, description}, index) => {
+                return(
+                    <SwiperSlide className="services__item card card-one">
+                        <span className="services__subtitle text-cs">{name}</span>
+                        
+                        <h3 className="services__title">{title}</h3>
+                        <p className="services__description">{description}</p>
+
+                        <a href="" className="link">See Pricing <FaArrowRight className='link__icon'></FaArrowRight></a>
+
+                        <img src={shapeTwo} alt="" className="shape c__shape" />
+                    </SwiperSlide>
+                )
+            })}
+
+        </Swiper>
+    </section>
+  )
+}
+
+export default Services;
