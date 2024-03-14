58055ef915f1c72da303ab4790d2bcf846988756
Senura Perera <senura.perera@arimaclanka.com>
2024-03-14 23:37:37 +0530

    Navbar toggle

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,56 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./header.css";
 import { links } from "../../Data";
 import { FaTwitter, FaInstagram } from "react-icons/fa";
 import { BsSun, BsMoon } from "react-icons/bs";
 
 const Header = () => {
-  return (
-    <header className="header">
-      <nav className="nav">
-        <a href="" className="nav__logo text-cs">
-          Senura
-        </a>
 
-        <div className="nav__menu">
-          <div className="nav__data">
-            <ul className="nav__list">
-              {links.map(({ name, path }, index) => {
-                return (
-                  <li className="nav__item" key={index}>
-                    <a href={path} className="nav__link text-cs">
-                      {name}
+    const [showMenu, setShowMenu] = useState(false);
+
+    // to hide the scrollbar when the nav is open
+    useEffect(() => {
+        document.body.classList.toggle('no-scroll', showMenu);
+    }, [showMenu]);
+
+    return (
+        <header className="header">
+        <nav className="nav">
+            <a href="" className="nav__logo text-cs">
+            Senura
+            </a>
+
+            <div className={`${showMenu ? 'nav__menu show-menu' : 'nav__menu'}`}>
+                <div className="nav__data">
+                    <ul className="nav__list">
+                    {links.map(({ name, path }, index) => {
+                        return (
+                        <li className="nav__item" key={index}>
+                            <a href={path} className="nav__link text-cs">
+                            {name}
+                            </a>
+                        </li>
+                        );
+                    })}
+                    </ul>
+                    <div className="header__socials">
+                    <a href="" className="header__social-link">
+                        <FaTwitter />
                     </a>
-                  </li>
-                );
-              })}
-            </ul>
-            <div className="header__socials">
-              <a href="" className="header__social-link">
-                <FaTwitter />
-              </a>
 
-              <a href="" className="header__social-link">
-                <FaInstagram />
-              </a>
+                    <a href="" className="header__social-link">
+                        <FaInstagram />
+                    </a>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
 
-        <div className="nav__btns">
-          <div className="theme__toggle">
-            <BsSun />
-          </div>
-          
-          <div className="nav__toggle">
-            <span></span>
-            <span></span>
-          </div>
-        </div>
+            <div className="nav__btns">
+                <div className="theme__toggle">
+                    <BsSun />
+                </div>
+                
+                <div className={`${showMenu ? 'nav__toggle animate-toggle' : 'nav__toggle'}`} onClick={() => {setShowMenu(!showMenu)}}>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
 
-      </nav>
-    </header>
-  );
+        </nav>
+        </header>
+    );
 };
 
 export default Header;
